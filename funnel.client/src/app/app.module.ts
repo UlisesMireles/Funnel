410d89022d435aca91ab7d6695de21ae88da5cf2
Misael Hernández <misael.hernandez@eisei.net.mx>
2025-02-06 15:40:59 -0600

    correcciones import prime ng

--- funnel.client/src/app/app.module.ts
+++ funnel.client/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +17,14 @@ import { AppComponent } from './app.component';
     BrowserModule, HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
