11b560f033f2022da8f3d874245b65e044a57681
UlisesMireles <ulises.mireles@eisei.net.mx>
2025-02-18 13:56:02 -0600

    ajustes assets url

--- funnel.client/src/enviroment/environment.prod.ts
+++ funnel.client/src/enviroment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseURL: "https://sfs-funnel.com/Tenant/",
-  baseURLAssets: "https://sfs-funnel.com/Tenant/"
+  baseURLAssets: "https://sfs-funnel.com/Tenant"
 };
--- funnel.client/src/enviroment/environment.qa.ts
+++ funnel.client/src/enviroment/environment.qa.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseURL: "https://sfs-funnel.com/Tenant-qa/",
-  baseURLAssets: "https://sfs-funnel.com/Tenant-qa/"
+  baseURLAssets: "https://sfs-funnel.com/Tenant-qa"
 };
