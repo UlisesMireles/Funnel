e96c0b6a73621e40322e8ba5dd2113903dddd166
Misael Hernández <misael.hernandez@eisei.net.mx>
2025-02-26 09:13:23 -0600

    cambio a formulario reactivo sectores

--- funnel.client/src/app/components/catalogos/sectores/modal-sectores/modal-sectores.component.ts
+++ funnel.client/src/app/components/catalogos/sectores/modal-sectores/modal-sectores.component.ts
@@ -10,6 +10,7 @@ import { SEL_Sectores } from '../../../../interfaces/Sector';
 
 // Services
 import { SectoresService } from '../../../../services/sectores.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-sectores',
@@ -19,13 +20,27 @@ import { SectoresService } from '../../../../services/sectores.service';
 })
 export class ModalSectoresComponent {
 
-  constructor(private sectoresService : SectoresService, private messageService: MessageService) { }
+  constructor(private sectoresService: SectoresService, private messageService: MessageService, private fb: FormBuilder) {
+    this.userId = parseInt(localStorage.getItem('currentUser')!);
+    this.form = this.fb.group({
+      idSector: [0],
+      bandera: ['INS-SECTOR'],
+      nombreSector: ['', Validators.required],
+      descripcionSector: ['', Validators.required],
+      idUsuarioCreador: [this.userId],
+      activo: [1]
+    });
+  }
+
   @Input() sector!: SEL_Sectores;
-  @Input() sectores: SEL_Sectores[]=[];
+  @Input() sectores: SEL_Sectores[] = [];
   @Input() title: string = 'Modal';
   @Input() visible: boolean = false;
   @Input() insertar: boolean = false;
   request!: requestSector;
+
+  form!: FormGroup;
+  userId: number = 0;
 
   sectorActivo: boolean = false;
   selectedLicencia: number | undefined;
@@ -36,112 +51,116 @@ export class ModalSectoresComponent {
 
   onDialogShow() {
     this.sectorActivo = this.sector?.desEstatusActivo === 'Activo';
-  }
-   close() {
-      this.visible = false;
-      this.visibleChange.emit(this.visible);
-      this.closeModal.emit();
+    if (this.insertar) {
+      this.form = this.fb.group({
+        idSector: [0],
+        bandera: ['INS-SECTOR'],
+        nombreSector: ['', Validators.required],
+        descripcionSector: ['', Validators.required],
+        idUsuarioCreador: [this.userId],
+        activo: [1]
+      });
+    } else {
+      this.form = this.fb.group({
+        idSector: [this.sector.idSector],
+        bandera: ['UPD-SECTOR'],
+        nombreSector: [this.sector.nombreSector, Validators.required],
+        descripcionSector: [this.sector.nombreSector, Validators.required],
+        idUsuarioCreador: [this.userId],
+        activo: [this.sectorActivo]
+      });
     }
-    actualizaSector() {
-        if (!this.request) {
-          this.request = {} as requestSector;
-        }
-        if (this.camposInvalidosEditar()) {
-          this.mostrarToastError();
-          return;
-        }
-        this.request.bandera = 'UPD-SECTOR';
-        this.request.idSector = this.sector.idSector;
-        this.request.nombreSector = this.sector.nombreSector;
-        this.request.descripcionSector= this.sector.descripcionSector;
-        this.request.idUsuarioCreador = parseInt(localStorage.getItem('currentUser') || '0', 10);
-        this.request.activo = this.sectorActivo ? 1 : 0;
+  }
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+    this.closeModal.emit();
+  }
+  actualizaSector() {
+    if (this.camposInvalidosEditar()) {
+      this.mostrarToastError();
+      return;
+    }
 
-        this.sectoresService.postINSUPDSector(this.request).subscribe(
-          {
-            next: (result: baseOut) => {
-              this.result.emit(result);
-              this.visible = false;
-              this.visibleChange.emit(this.visible);
-              this.closeModal.emit();
-            },
-            error: (error: baseOut)=> {
-              this.result.emit(error);
-              this.visible = false;
-              this.visibleChange.emit(this.visible);
-              this.closeModal.emit();
-            }
-          }
-        );
+    this.form.controls['activo'].setValue(this.form.controls['activo'].value ? 1 : 0);
+    this.sectoresService.postINSUPDSector(this.form.value).subscribe(
+      {
+        next: (result: baseOut) => {
+          this.result.emit(result);
+          this.visible = false;
+          this.visibleChange.emit(this.visible);
+          this.closeModal.emit();
+        },
+        error: (error: baseOut) => {
+          this.result.emit(error);
+          this.visible = false;
+          this.visibleChange.emit(this.visible);
+          this.closeModal.emit();
+        }
       }
-      guardarSector(){
-        if (!this.request) {
-          this.request = {} as requestSector;
+    );
+  }
+  guardarSector() {
+    if (this.camposInvalidosInsertar()) {
+      this.mostrarToastError();
+      return;
+    }
+    this.sectoresService.postINSUPDSector(this.form.value).subscribe(
+      {
+        next: (result: baseOut) => {
+          this.result.emit(result);
+          this.visible = false;
+          this.visibleChange.emit(this.visible);
+          this.closeModal.emit();
+        },
+        error: (error: baseOut) => {
+          this.result.emit(error);
+          this.visible = false;
+          this.visibleChange.emit(this.visible);
+          this.closeModal.emit();
         }
-        if (this.camposInvalidosInsertar()) {
-          this.mostrarToastError();
-          return;
-        }
-        this.request.bandera = 'INS-SECTOR';
-        this.request.idSector = this.sector.idSector;
-        this.request.nombreSector = this.sector.nombreSector;
-        this.request.descripcionSector= this.sector.descripcionSector;
-        this.request.idUsuarioCreador = parseInt(localStorage.getItem('currentUser') || '0', 10);
-        this.request.activo = this.sectorActivo ? 1 : 0;
-        this.sectoresService.postINSUPDSector(this.request).subscribe(
-          {
-            next: (result: baseOut) => {
-              this.result.emit(result);
-              this.visible = false;
-              this.visibleChange.emit(this.visible);
-              this.closeModal.emit();
-            },
-            error: (error: baseOut)=> {
-              this.result.emit(error);
-              this.visible = false;
-              this.visibleChange.emit(this.visible);
-              this.closeModal.emit();
-            }
-          }
-        );
+      }
+    );
+  }
+  validarSector(): boolean {
+    const nombreSector = this.form.get('nombreSector')?.value;
+    if (this.sectores.some(sector => sector.nombreSector?.toLowerCase().trim() === nombreSector.toLowerCase().trim())) {
+      return false;
+    }
+    return true;
+  }
+  esCampoInvalido(campo: string): boolean {
+    const control = this.form.get(campo);
+    return control ? (control.invalid && (control.dirty || control.touched)) : false;
+  }
 
-      }
-      validarSector(): boolean {
-        if (this.sectores.some(sector => sector.nombreSector?.toUpperCase() === this.sector.nombreSector?.toUpperCase())) {
-          return false;
-        }
-        return true;
-      }
-      esCampoInvalido(valor: any): boolean {
-        return valor === null || valor === undefined || valor === '' || valor <= 0;
-      }
-      camposInvalidosInsertar(): boolean {
-        return (
-          this.esCampoInvalido(this.sector.nombreSector) ||
-          this.esCampoInvalido(this.sector.descripcionSector)||
-          !this.validarSector()
-        );
-      }
-      camposInvalidosEditar(): boolean {
-        return (
-          this.esCampoInvalido(this.sector.nombreSector) ||
-          this.esCampoInvalido(this.sector.descripcionSector)
-        );
-      }
+  camposInvalidosInsertar(): boolean {
+    return (
+      this.esCampoInvalido('nombreSector') ||
+      this.esCampoInvalido('descripcionSector') ||
+      !this.validarSector()
+    );
+  }
+  camposInvalidosEditar(): boolean {
+    return (
+      this.esCampoInvalido('nombreSector') ||
+      this.esCampoInvalido('descripcionSector')
+    );
+  }
 
-      /**
-       * Método para mostrar un toast de error cuando hay campos vacíos.
-       */
-      mostrarToastError() {
-        let mensaje='Es Necesario llenar los campos indicados.';
-        this.messageService.clear();
-        if (!this.validarSector() && this.insertar) {
-          mensaje = 'El Sector ya existe.';
-        }
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: mensaje,
-        });
-      }
+  /**
+   * Método para mostrar un toast de error cuando hay campos vacíos.
+   */
+  mostrarToastError() {
+    let mensaje = 'Es Necesario llenar los campos indicados.';
+    this.messageService.clear();
+    if (!this.validarSector() && this.insertar) {
+      mensaje = 'El Sector ya existe.';
+    }
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: mensaje,
+    });
+  }
 }
